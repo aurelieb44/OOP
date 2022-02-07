87839e87cfc34b8c8bd048de47ea40198621b5aa
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-02-07 16:29:15 -0600

    commit

--- Fly.py
+++ Fly.py
@@ -1,15 +1,11 @@
 import InsectClass as i 
 
 def main():
-       my_insect = i.Insect()   
+    my_insect = i.Insect()   
 
-       print('The flight is: ', my_insect.get_flight(), ' miles')    
+    my_insect.flight_length()
 
-       print('I am going to do it ten times:')
-       for count in range(10):
-           my_insect.flight()
-
-           print('The flight is: ', my_insect.get_flight(), ' miles')  
+    print('The flight is: ', my_insect.get_flight(), ' miles')  
 
 
 main()
