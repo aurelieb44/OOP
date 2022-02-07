f8c5f68b15a1489dd0ac0ef04c4ec5f25ced4ee8
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-02-07 13:56:18 -0600

    commit changes

--- Coin Toss.py
+++ Coin Toss.py
@@ -1,13 +1,21 @@
-import CoinClass as c
+#actually creating a coin
 
+import CoinClass as c #using an alias c
+# Importing the .py that is the object definition file. 
+# I'm not importing the name of the file but the name of the file.
 
 # The main function.
 def main():
        # Create an object from the Coin class.
-       my_coin = c.Coin()   # this creates an instance called 'my_coin' of the class 'Coin()'
+       my_coin = c.Coin()   
+       # this creates an instance called 'my_coin' of the class 'Coin()'
+       # my_coin is a instance/object of the coin class
+       # All the methods are available to you at the instance
+       # You wouldn't use Coin naymore, but my_coin.
 
        # Display the side of the coin that is facing up.
        print('This side is up:', my_coin.get_sideup())    # notice you do not have to supply the argument/parameter
+       # when we create a new instance, the value is set to head by default.
 
        # Toss the coin.
        print('I am going to toss the coin ten times:')
--- CoinClass.py
+++ CoinClass.py
@@ -1,3 +1,6 @@
+#The class definition file
+#The blueprint of how to create an object/coin
+
 import random
 
 # The Coin class simulates a coin that can
@@ -14,9 +17,11 @@ class Coin:
     # in the range of 0 through 1. If the number
     # is 0, then sideup is set to 'Heads'.
     # Otherwise, sideup is set to 'Tails'.
+    # we don't want the user to change the value of sideup. 
+    # we only allow the user to change the value through toss.
 
     def toss(self):
-        if random.randint(0, 1) == 0:
+        if random.randint(0, 1) == 0: #randomly picks 0 or 1
             self.sideup = 'Heads'
         else:
             self.sideup = 'Tails'
@@ -26,3 +31,4 @@ class Coin:
 
     def get_sideup(self):
             return self.sideup
+    #returns the value of our attribute called sideup.
